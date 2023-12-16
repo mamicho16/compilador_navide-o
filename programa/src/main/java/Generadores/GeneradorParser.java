9bb698b3c8904fb8c4a1aa8e76e9827a9efa0427
Mauricio <mamichofer16@gmail.com>
2023-12-15 18:05:04 -0600

    Arreglo de repo

---
+++ programa/src/main/java/Generadores/GeneradorParser.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Generadores;
+
+/**
+ *
+ * @author anon
+ */
+public class GeneradorParser {
+    
+   public static void main(String[] args)
+    {
+        String opciones[] = new String[3]; 
+        
+        //Seleccionamos la opción de dirección de destino
+        opciones[0] = "-destdir";
+        
+        //Le damos la dirección, carpeta donde se va a generar el parser.java & el simbolosxxx.java
+        opciones[1] = "src/Analizadores";
+       
+        //Seleccionamos la opcion de clase parser
+        opciones[2] = "-parser";         
+        
+        //Le damos nombre a esa clase del paso anterior
+        opciones[3] = "Parser"; 
+        
+        //Le decimos donde se encuentra el archivo .cup 
+        opciones[4] = "src/Analizadores/parser.cup"; 
+        
+        try 
+        {
+            java_cup.Main.main(opciones);
+        } 
+        catch (Exception ex)
+        {
+            System.out.print(ex);
+        }
+    }
+}
+
---
+++ programa/src/main/java/Programa/App.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Programa;
+
+/**
+ *
+ * @author anon
+ */
+public class App {
+    
+    public static void main(String[] args) {
+        // Aqui van las funciones para correr el programa principal
+        System.out.println("Hola mundo");
+    }
+   
+}
